bd0af0ba4f93aa1a251846754576d29d3db71862
Imran25_1_test <imransarkar.dev@gmail.com>
2025-10-03 04:27:01 +0600

    used lottie in register.jsx

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,5 +1,6 @@
+import Lottie from "lottie-react";
 import React from "react";
-
+import registerLottie from "../../assets/Lotties/register.json";
 const Register = () => {
   return (
     <div className="flex">
@@ -26,26 +27,33 @@ const Register = () => {
         <div className="hero bg-base-200 min-h-screen">
           <div className="hero-content flex-col lg:flex-row-reverse">
             <div className="text-center lg:text-left">
-        
-
+              <Lottie loop={true} animationData={registerLottie}></Lottie>
             </div>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
               <div className="card-body">
                 <h1 className="text-5xl font-bold">Login now!</h1>
-                <fieldset className="fieldset">
-                  <label className="label">Email</label>
-                  <input type="email" className="input" placeholder="Email" />
-                  <label className="label">Password</label>
-                  <input
-                    type="password"
-                    className="input"
-                    placeholder="Password"
-                  />
-                  <div>
-                    <a className="link link-hover">Forgot password?</a>
-                  </div>
-                  <button className="btn btn-neutral mt-4">Login</button>
-                </fieldset>
+                <form>
+                  <fieldset className="fieldset">
+                    <label className="label">Email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      className="input"
+                      placeholder="Email"
+                    />
+                    <label className="label">Password</label>
+                    <input
+                      type="password"
+                      className="input"
+                      placeholder="Password"
+                      name="password"
+                    />
+                    <div>
+                      <a className="link link-hover">Forgot password?</a>
+                    </div>
+                    <button className="btn btn-neutral mt-4">Login</button>
+                  </fieldset>
+                </form>
               </div>
             </div>
           </div>
