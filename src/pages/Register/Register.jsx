e7a9085a8dbb3b6fac6f45a23573cf2687c42093
Imran25_1_test <imransarkar.dev@gmail.com>
2025-10-03 03:33:39 +0600

    added some pages and components like register home

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="flex">
+      <div className="flex-1">
+        <div className="hero bg-base-200 min-h-screen">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+              className="max-w-sm rounded-lg shadow-2xl"
+            />
+            <div>
+              <h1 className="text-5xl font-bold">Box Office News!</h1>
+              <p className="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex-1">
+        <div className="hero bg-base-200 min-h-screen">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left">
+        
+
+            </div>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+              <div className="card-body">
+                <h1 className="text-5xl font-bold">Login now!</h1>
+                <fieldset className="fieldset">
+                  <label className="label">Email</label>
+                  <input type="email" className="input" placeholder="Email" />
+                  <label className="label">Password</label>
+                  <input
+                    type="password"
+                    className="input"
+                    placeholder="Password"
+                  />
+                  <div>
+                    <a className="link link-hover">Forgot password?</a>
+                  </div>
+                  <button className="btn btn-neutral mt-4">Login</button>
+                </fieldset>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,17 +1,22 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../Layouts/RootLayout";
 import Home from "../pages/Home/Home";
+import Register from './../pages/Register/Register';
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayout,
     children: [
-        {
-            index : true,
-            Component : Home
-        }
-    ]
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/register",
+        Component: Register,
+      },
+    ],
   },
 ]);
 
